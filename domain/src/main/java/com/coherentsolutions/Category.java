7c16d70c72048e8097616a132d9c8a952af1850f
IrinaKorobeynik <irinakorobeynik@coherentsolutions.com>
2023-05-04 12:27:56 +0300

    initial implementation of domain classes

--- domain/src/main/java/com/coherentsolutions/Category.java
+++ domain/src/main/java/com/coherentsolutions/Category.java
@@ -11,5 +11,28 @@ public abstract class Category {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", type=" + type +
+                ", productList=" + productList +
+                '}';
+    }
 }
