103524b61e2cd810e0c09feff107b8ae7990138a
IrinaKorobeynik <irinakorobeynik@coherentsolutions.com>
2023-05-12 16:11:34 +0300

    adding method to add product to the category

--- store/src/main/java/com/coherentsolutions/store/RandomStorePopulator.java
+++ store/src/main/java/com/coherentsolutions/store/RandomStorePopulator.java
@@ -1,14 +1,38 @@
 package com.coherentsolutions.store;
 
+import com.coherentsolutions.domain.Category;
+import com.coherentsolutions.domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.apache.commons.lang3.RandomUtils.nextInt;
+
 public class RandomStorePopulator {
 
     private Store store;
+
     public RandomStorePopulator(Store store) {
         this.store = store;
     }
 
-    public void FillStore(){
+    public void FillStoreWithProduct() {
+        RandomProductGenerator randomProductGenerator = new RandomProductGenerator();
+        int counter = nextInt(0, 10);
+        List<Category> categoryList = new ArrayList<>();
+        for (Category category : categoryList) {
+            for (int i = 0; i < counter; i++) {
+                Product product = new Product(randomProductGenerator.generateName(category.getName()),
+                        randomProductGenerator.generateRate(),
+                        randomProductGenerator.generatePrice());
+                category.addProduct(product);
+            }
+        }
 
+    }
+
+    public void FillStoreWithCategories() {
 
     }
 }
