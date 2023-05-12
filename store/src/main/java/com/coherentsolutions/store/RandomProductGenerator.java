9ab6bd9aedd14b48433a582e6310df6ec06f29d6
IrinaKorobeynik <irinakorobeynik@coherentsolutions.com>
2023-05-12 13:13:27 +0300

    Updates made based on comment under previous pull request

--- store/src/main/java/com/coherentsolutions/store/RandomProductGenerator.java
+++ store/src/main/java/com/coherentsolutions/store/RandomProductGenerator.java
@@ -6,7 +6,7 @@ import com.github.javafaker.Faker;
 public class RandomProductGenerator {
     private Faker faker = new Faker();
 
-    public String setName(String category) {
+    public String generateName(String category) {
         switch (category) {
             case "Book":
                 return faker.book().title();
@@ -19,11 +19,11 @@ public class RandomProductGenerator {
         }
     }
 
-    public int setPrice(Product product) {
+    public int generatePrice(Product product) {
         return faker.number().numberBetween(1, 1000);
     }
 
-    public int setRate(Product product) {
+    public int generateRate(Product product) {
         return faker.number().numberBetween(1, 10);
     }
 
